cbb340e343d422b55f367ec20bf1ce75b9116d57
Og B. Maciel <omaciel@ogmaciel.com>
2014-08-22 15:39:11 -0400

    Make `resize2fs` work non-interactively.
    BY passing `-f` to `resize2fs` we avoid having to confirm any changes,
    as shown below:
    
    ```bash
    Do you really want to remove active logical volume home? [y/n]:
    ```

--- fabfile.py
+++ fabfile.py
@@ -321,7 +321,7 @@ def partition_disk():
     run('umount /home')
     run('lvremove /dev/mapper/*home')
     run('lvresize -l +100%FREE /dev/mapper/*root')
-    run('if uname -r | grep -q el6; then resize2fs /dev/mapper/*root; '
+    run('if uname -r | grep -q el6; then resize2fs -f /dev/mapper/*root; '
         'else xfs_growfs /; fi')
 
 
