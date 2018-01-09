b9866c61343119e05d4aead6b933a258d2917704
ntkathole <nikhilkathole2683@gmail.com>
2018-01-09 20:02:42 +0530

    Added task for creating discovery image

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     package_dir={'automation_tools': 'automation_tools'},
     include_package_data=True,
     install_requires=[
+        'BeautifulSoup',
         'Fabric3',
         'lxml',
         'pycurl',
