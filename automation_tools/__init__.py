71cfbf1efa76512528ba56578ac29f2d414bd9c9
Vijay Singh <vijsingh@redhat.com>
2019-08-02 17:43:10 +0530

    Addressed comments

--- automation_tools/__init__.py
+++ automation_tools/__init__.py
@@ -3034,9 +3034,8 @@ def setenforce(mode):
     run('setenforce {0}'.format(mode))
 
 
-def download_manifest(url=None, consumer=None, pool_id=None, count=None):
-    """Task for downloading the manifest file from customer portal,
-    also attach subscription to it if requires.
+def download_manifest(url=None, consumer=None):
+    """Task for downloading the manifest file from customer portal.
 
     The following environment variables affect this command:
 
@@ -3051,8 +3050,6 @@ def download_manifest(url=None, consumer=None, pool_id=None, count=None):
 
     :param url: Subscription Manager URL
     :param consumer: A consumer hash to be used for getting the manifest
-    :param pool_id: A pool ID of subscription
-    :param count: A quantity of subscription requires to attach
     :returns: a path string to a downloaded manifest file
     """
     user = os.environ.get('RHN_USERNAME')
@@ -3070,39 +3067,28 @@ def download_manifest(url=None, consumer=None, pool_id=None, count=None):
         url = os.environ.get('SM_URL')
     if consumer is None:
         consumer = os.environ.get('CONSUMER')
+    manifest_file = run('mktemp --suffix=.zip')
 
-    if pool_id and count:
-        command = ('curl -sk -X POST -H "Authorization:Basic {0}"'
-                   ' "{1}/subscription/consumers/{2}/entitlements?pool={3}&quantity={4}"').format(
-            base64string.decode('utf-8'), url, consumer, pool_id, count)
-        response = run(command)
-        if 'updated' in response:
-            return True
-        else:
-            return False
+    # we do this as we would otherwise potentially download a manifest which
+    # has new metadata (such as new content sets/repos) but does not have the
+    # required Entitlement certificates to actually access the content.
+    certs_put = ('curl -sk -X PUT -H "Authorization:Basic {0}"'
+                 ' {1}/subscription/consumers/{2}/certificates'
+                 '?lazy_regen=false').format(
+                 base64string.decode('utf-8'), url, consumer)
+    run(certs_put)
+
+    command = ('curl -sk -H "Authorization:Basic {0}"'
+               ' {1}/subscription/consumers/{2}/export/').format(
+               base64string.decode('utf-8'), url, consumer)
+
+    response = run(command + ' -I')
+    if ('Content-Disposition: attachment' in response):
+        run(command + ' -o {0}'.format(manifest_file))
+        return manifest_file
     else:
-        manifest_file = run('mktemp --suffix=.zip')
-
-        # we do this as we would otherwise potentially download a manifest which
-        # has new metadata (such as new content sets/repos) but does not have the
-        # required Entitlement certificates to actually access the content.
-        certs_put = ('curl -sk -X PUT -H "Authorization:Basic {0}"'
-                     ' {1}/subscription/consumers/{2}/certificates'
-                     '?lazy_regen=false').format(
-                     base64string.decode('utf-8'), url, consumer)
-        run(certs_put)
-
-        command = ('curl -sk -H "Authorization:Basic {0}"'
-                   ' {1}/subscription/consumers/{2}/export/').format(
-                   base64string.decode('utf-8'), url, consumer)
-
-        response = run(command + ' -I')
-        if ('Content-Disposition: attachment' in response):
-            run(command + ' -o {0}'.format(manifest_file))
-            return manifest_file
-        else:
-            raise ValueError('Request has returned no attachment. Check the'
-                             ' session and distributor hash')
+        raise ValueError('Request has returned no attachment. Check the'
+                         ' session and distributor hash')
 
 
 def validate_manifest(manifest_file):
@@ -3112,16 +3098,18 @@ def validate_manifest(manifest_file):
         List of subscriptions which are supposed to be in the manifest (one
         subscription name per line). No other subscription is allowed.
     :param manifest_file: Where is the manifest to investigate.
+    :return: list with True/False boolean
     """
     exp_subs_file = os.environ.get('EXP_SUBS_FILE', None)
     with open(exp_subs_file, 'r') as fp:
         exp_subs = {}
-        exp_subs_details = [r.strip() for r in fp.readlines() if not r.strip().startswith('#')]
-        for name in exp_subs_details:
-            exp_subs[name.split(';')[0]] = [name.split(';')[1], name.split(';')[2]]
+        exp_subs_details = [r.strip().split(';') for r in fp.readlines() if not r.strip(
+        ).startswith('#')]
+        for name_list in exp_subs_details:
+            exp_subs[name_list[0]] = name_list[1:]
 
-    print("expected subscriptions in %s are %s" % (manifest_file, ' & '.join(exp_subs.keys())))
-    rct_output = run("rct cat-manifest --no-content %s" % manifest_file).split("\n")
+    print("expected subscriptions in {} are {}".format(manifest_file, ' & '.join(exp_subs.keys())))
+    rct_output = run("rct cat-manifest --no-content {}".format(manifest_file)).split("\n")
     sub_name_next = False
     current_subs = list()
     for r in rct_output:
@@ -3133,21 +3121,65 @@ def validate_manifest(manifest_file):
             if r.startswith('Name: '):
                 current_subs.append(r[6:])
             sub_name_next = False
-    print("current subscriptions in %s are %s" % (manifest_file, ' & '.join(current_subs)))
+    print("current subscriptions in {} are {}".format(manifest_file, ' & '.join(current_subs)))
 
     response = [True]
     for sub in exp_subs.keys():
-        if sub in current_subs:
-            continue
-        else:
+        if sub not in current_subs:
             response.append('Yes')
-            print("Attaching missing subscription %s" % (sub))
-            attach = download_manifest(pool_id=exp_subs[sub][0], count=exp_subs[sub][1])
+            print("Attaching missing subscription {}".format(sub))
+            attach = attach_subscription(pool_id=exp_subs[sub][0], count=exp_subs[sub][1])
             if attach:
-                print("Successfully attached subscription %s" % (sub))
+                print("Successfully attached subscription {}" .format(sub))
             else:
                 return [False]
     return response
+
+
+def attach_subscription(url=None, consumer=None, pool_id=None, count=None):
+    """Task to attach subscription to manifest.
+
+    The following environment variables affect this command:
+
+    SM_URL
+      Subscription Manager URL (e.g. 'https://subscription.rhsm.redhat.com')
+    CONSUMER
+        A consumer hash to be used for getting the manifest
+    RHN_USERNAME
+        Red Hat Network username
+    RHN_PASSWORD
+        Red Hat Network password
+
+    :param url: Subscription Manager URL
+    :param consumer: A consumer hash to be used for getting the manifest
+    :param pool_id: A pool ID of subscription
+    :param count: A quantity of subscription requires to attach
+    :returns: boolean True/False
+    """
+    user = os.environ.get('RHN_USERNAME')
+    password = os.environ.get('RHN_PASSWORD')
+    auth_details = u'{0}:{1}'.format(user, password)
+
+    if isinstance(auth_details, str):  # py3
+        bytestring = bytes('{0}:{1}'.format(user, password), 'utf-8')
+    else:  # py2
+        bytestring = bytes('{0}:{1}'.format(user, password))
+
+    base64string = base64.encodestring(bytestring).strip()
+
+    if url is None:
+        url = os.environ.get('SM_URL')
+    if consumer is None:
+        consumer = os.environ.get('CONSUMER')
+
+    if pool_id and count:
+        command = ('curl -sk -X POST -H "Authorization:Basic {0}"'
+                   ' "{1}/subscription/consumers/{2}/entitlements?pool={3}&quantity={4}"').format(
+            base64string.decode('utf-8'), url, consumer, pool_id, count)
+        response = run(command)
+        return 'updated' in response
+    else:
+        raise ValueError("Pass values under pool_id and count")
 
 
 def relink_manifest(manifest_file=None):
