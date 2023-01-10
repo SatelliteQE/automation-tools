de090390670a0c9cdb5148137a88d0ace00116de
Lukas Pramuk <lpramuk@redhat.com>
2023-01-10 17:49:16 +0100

    Switch to original Fabric 1.x

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'beautifulsoup4',
-        'Fabric3',
+        'Fabric<2',
         'lxml',
         'pycurl',
         'pytest',
